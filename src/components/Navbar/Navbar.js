8a40c2910fe135d911835d0a6e01fff1050b2624
dungnguyenit <dungnguyenze13@gmail.com>
2022-03-28 15:58:19 +0700

    them

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,44 +1,51 @@
-import React,{useState} from 'react';
-import { Link } from 'react-router-dom';
-import '../style/navbar.scss';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../style/navbar.scss";
 
 const Navbar = () => {
-   const [ isMobile,setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
   return (
     <div className="navbar">
       <div className="nav-logo">
-      <img src={require('../image/Logo.png')} alt="logo"
-      className="logo"/>
+        <img src={require("../image/Logo.png")} alt="logo" className="logo" />
       </div>
-      
-      <ul className={isMobile? "nav-links-mobile" :"nav-links"}
-      onClick={() =>setIsMobile(false)}
+
+      <ul
+        className={isMobile ? "nav-links-mobile" : "nav-links"}
+        onClick={() => setIsMobile(false)}
       >
-      <Link to='/' className="home">
-        <li>HOME</li>
-      </Link>
-      <Link to='Shop' className="shop">
-        <li>SHOP</li>
-      </Link>
-      <Link to='Magazine' className="magazine">
-        <li> MAGAZINE</li>
-      </Link>
-      <Link to='Login' className="Login">
-        <li>LOGIN</li>
-      </Link>
+        <Link to="/" className="home">
+          <li>HOME</li>
+        </Link>
+        <Link to="Shop" className="shop">
+          <li>SHOP</li>
+        </Link>
+        <Link to="Magazine" className="magazine">
+          <li> MAGAZINE</li>
+        </Link>
       </ul>
       <div className="nav-cart">
-      <img src={require('../image/Cart Button.png')} alt="logo"
-      className="logo"/>
-      
+        <img
+          src={require("../image/Add to Cart Button.png")}
+          alt="logo"
+          className="logo"
+        />
+        <Link to="Login" className="Login">
+          <li>LOGIN</li>
+        </Link>
       </div>
-      <button className='mobile-menu-icon'
-      onClick={() =>setIsMobile(!isMobile)}
+      <button
+        className="mobile-menu-icon"
+        onClick={() => setIsMobile(!isMobile)}
       >
-        {isMobile? <i className="fas fa-times"></i> :<i className="fas fa-bars">  </i>}
+        {isMobile ? (
+          <i className="fas fa-times"></i>
+        ) : (
+          <i className="fas fa-bars"> </i>
+        )}
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
